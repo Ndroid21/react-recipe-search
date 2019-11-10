fdbd5dedf3070c4925fef49e0c2aab390a69a8d2
Nishant Kashyap <nishant.kashyap2104@gmail.com>
2019-11-10 22:23:18 +0530

    added Recipe and RecipeDetails component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import React, { Component } from 'react';
-import RecipeSearch from './components/RecipeSearch';
-import RecipeList from './components/RecipeList';
-
-import { recipes } from './tempList';
+import React, { Component } from "react";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { recipes } from "./tempList";
 
-export default class App extends Component {
+class App extends Component {
   state = {
-    recipes: recipes
+    recipes: recipes,
+    url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+      }`,
+    base_url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+      }`,
+    details_id: 35389,
+    pageIndex: 1,
+    search: "",
+    query: "&q=",
+    error: ""
+  };
+
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      console.log(jsonData);
+      if (jsonData.recipes.length === 0) {
+        this.setState(() => {
+          return { error: "sorry, but your search did not return any results" };
+        });
+      } else {
+        this.setState(() => {
+          return { recipes: jsonData.recipes, error: "" };
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayPage = index => {
+    switch (index) {
+      default:
+      case 1:
+        return (
+          <RecipeList
+            recipes={this.state.recipes}
+            handleDetails={this.handleDetails}
+            value={this.state.search}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            error={this.state.error}
+          />
+        );
+      case 0:
+        return (
+          <RecipeDetails
+            id={this.state.details_id}
+            handleIndex={this.handleIndex}
+          />
+        );
+    }
+  };
+
+  handleIndex = index => {
+    this.setState({
+      pageIndex: index
+    });
+  };
+  handleDetails = (index, id) => {
+    this.setState({
+      pageIndex: index,
+      details_id: id
+    });
+  };
+  handleChange = e => {
+    this.setState(
+      {
+        search: e.target.value
+      },
+      () => {
+        // console.log(this.state.search);
+      }
+    );
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      () => {
+        return { url: `${base_url}${query}${search}`, search: "" };
+      },
+      () => {
+        this.getRecipes();
+      }
+    );
+  };
 
   render() {
     return (
-      <React.Fragment>
-        <RecipeSearch />
-        <RecipeList recipes={this.state.recipes} />
-      </React.Fragment>
+      <React.Fragment>{this.displayPage(this.state.pageIndex)}</React.Fragment>
     );
   }
 }
 
+export default App;
